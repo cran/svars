3449724138db023953f4fce34f4ad1c830a75f7f
Alexander Lange <alexander.lange@uni-goettingen.de>
2019-06-28 14:00:03 +0000

    version 1.3.0

--- tests/testthat/test-id_ngml.R
+++ tests/testthat/test-id_ngml.R
@@ -55,6 +55,82 @@ test_that("id.ngml and boot 2-dim stage3 = FALSE works, no restriction", {
   expect_match(x1$method, x2$method, "Non-Gaussian maximum likelihood")
 })
 
+test_that("id.ngml and boot 2-dim stage3 = FALSE works, no restriction constant + trend", {
+  skip_on_cran()
+  set.seed(23211)
+  v1 <- vars::VAR(USA[,-3], p = 3, type = 'both' )
+  x1 <- id.ngml(v1)
+  x2 <- id.ngml_boot(v1)
+
+  expect_equal(round(x1$Lik, 4),round(x2$Lik, 4), -422.4783)
+  expect_equal(sum(round(x1$B, 4)), sum(round(x2$B, 4)), 1.7762)
+  expect_equal(round(sum(x1$df),4),round(sum(x2$df),4), 8.2167)
+
+  expect_equal(x1$K, x2$K, 2)
+  expect_equal(x1$n, x2$n, 172)
+  expect_equal(x1$stage3, x2$stage3, FALSE)
+  expect_equal(x1$p,x2$p, 3)
+
+  expect_match(x1$method, x2$method, "Non-Gaussian maximum likelihood")
+})
+
+test_that("id.ngml and boot 2-dim stage3 = FALSE works, no restriction without deterministic term", {
+  skip_on_cran()
+  set.seed(23211)
+  v1 <- vars::VAR(USA[,-3], p = 3, type = 'none' )
+  x1 <- id.ngml(v1)
+  x2 <- id.ngml_boot(v1)
+
+  expect_equal(round(x1$Lik, 4),round(x2$Lik, 4), -426.3248)
+  expect_equal(sum(round(x1$B, 4)), sum(round(x2$B, 4)), 1.8878)
+  expect_equal(round(sum(x1$df),4),round(sum(x2$df),4), 7.6745)
+
+  expect_equal(x1$K, x2$K, 2)
+  expect_equal(x1$n, x2$n, 172)
+  expect_equal(x1$stage3, x2$stage3, FALSE)
+  expect_equal(x1$p,x2$p, 3)
+
+  expect_match(x1$method, x2$method, "Non-Gaussian maximum likelihood")
+})
+
+test_that("id.ngml and boot 2-dim stage3 = TRUE works, no restriction constant + trend", {
+  skip_on_cran()
+  set.seed(23211)
+  v1 <- vars::VAR(USA[,-3], p = 3, type = 'both')
+  x1 <- id.ngml(v1, stage3 = TRUE)
+  x2 <- id.ngml_boot(v1, stage3 = TRUE)
+
+  expect_equal(round(x1$Lik, 4),round(x2$Lik, 4), -422.4783)
+  expect_equal(sum(round(x1$B, 4)), sum(round(x2$B, 4)), 1.7762)
+  expect_equal(round(sum(x1$df),4),round(sum(x2$df),4), 8.2167)
+
+  expect_equal(x1$K, x2$K, 2)
+  expect_equal(x1$n, x2$n, 172)
+  expect_equal(x1$stage3, x2$stage3, FALSE)
+  expect_equal(x1$p,x2$p, 3)
+
+  expect_match(x1$method, x2$method, "Non-Gaussian maximum likelihood")
+})
+
+test_that("id.ngml and boot 2-dim stage3 = TRUE works, no restriction without deterministic term", {
+  skip_on_cran()
+  set.seed(23211)
+  v1 <- vars::VAR(USA[,-3], p = 3, type = 'none' )
+  x1 <- id.ngml(v1, stage3 = TRUE)
+  x2 <- id.ngml_boot(v1, stage3 = TRUE)
+
+  expect_equal(round(x1$Lik, 4),round(x2$Lik, 4), -426.3248)
+  expect_equal(sum(round(x1$B, 4)), sum(round(x2$B, 4)), 1.8878)
+  expect_equal(round(sum(x1$df),4),round(sum(x2$df),4), 7.6745)
+
+  expect_equal(x1$K, x2$K, 2)
+  expect_equal(x1$n, x2$n, 172)
+  expect_equal(x1$stage3, x2$stage3, FALSE)
+  expect_equal(x1$p,x2$p, 3)
+
+  expect_match(x1$method, x2$method, "Non-Gaussian maximum likelihood")
+})
+
 test_that("id.ngml 2-dim stage3 = TRUE works, no restriction", {
   skip_on_cran()
   set.seed(23211)
@@ -62,7 +138,7 @@ test_that("id.ngml 2-dim stage3 = TRUE works, no restriction", {
   x1 <- id.ngml(v1, stage3 = TRUE)
   x2 <- id.ngml_boot(v1, stage3 = TRUE)
 
-  expect_equal(round(x1$Lik, 4),round(x2$Lik, 4), -425.5546)
+  expect_equal(round(x1$Lik, 4), round(x2$Lik, 4), -425.5546)
   expect_equal(sum(round(x1$B, 4)), sum(round(x2$B, 4)), 1.8334)
   expect_equal(round(sum(x1$df),4), round(sum(x2$df),4), 7.8661)
 
@@ -83,7 +159,7 @@ test_that("id.ngml and id.ngml_boot 3-dim stage3 = FALSE works, with restriction
   restriction_matrix[2, 3] <- 0
   x1 <- id.ngml(v1, restriction_matrix = restriction_matrix)
   x2 <- id.ngml_boot(v1, restriction_matrix = restriction_matrix)
-  expect_equal(round(x1$Lik, 4),round(x2$Lik, 4), 561.8295)
+  expect_equal(round(x1$Lik, 4), round(x2$Lik, 4), 561.8295)
   expect_equal(sum(round(x1$B, 4)), sum(round(x2$B, 4)), 2.6431)
   expect_equal(round(sum(x1$df),4), round(sum(x2$df),4), 16.8249)
 
@@ -91,6 +167,8 @@ test_that("id.ngml and id.ngml_boot 3-dim stage3 = FALSE works, with restriction
   expect_equal(x1$n,x2$n, 169)
   expect_equal(x1$stage3,x2$stage3, FALSE)
   expect_equal(x1$p,x2$p, 6)
+
+  expect_gt(x1$lRatioTest$`Test statistic`, 0)
 
   expect_match(x1$method,x2$method, "Non-Gaussian maximum likelihood")
 })
@@ -104,9 +182,11 @@ test_that("id.ngml and boot 3-dim stage3 = TRUE works, with restriction", {
   restriction_matrix[2, 3] <- 0
   x1 <- id.ngml(v1, stage3 =  TRUE, restriction_matrix = restriction_matrix)
   x2 <- id.ngml_boot(v1, stage3 = TRUE, restriction_matrix = restriction_matrix)
-  expect_equal(round(x1$Lik, 4),round(x2$Lik, 4), -561.8295)
+  expect_equal(round(x1$Lik, 4), round(x2$Lik, 4), -561.8295)
   expect_equal(sum(round(x1$B, 4)),sum(round(x2$B, 4)), 2.6431)
   expect_equal(round(sum(x1$df),4),round(sum(x2$df),4), 16.8249)
+
+  expect_gt(x1$lRatioTest$`Test statistic`, 0)
 
   expect_equal(x1$K, x2$K, 3)
   expect_equal(x1$n, x2$n, 169)
@@ -125,9 +205,11 @@ test_that("id.ngml and boot 2-dim stage3 = FALSE works, with restriction", {
   x1 <- id.ngml(v1, restriction_matrix = restriction_matrix)
   x2 <- id.ngml_boot(v1, restriction_matrix = restriction_matrix)
 
-  expect_equal(round(x1$Lik, 4),round(x2$Lik, 4), -433.6669)
+  expect_equal(round(x1$Lik, 4), round(x2$Lik, 4), -433.6669)
   expect_equal(sum(round(x1$B, 4)), sum(round(x2$B, 4)), 1.8721)
   expect_equal(round(sum(x1$df),4), round(sum(x2$df),4), 8.3969)
+
+  expect_gt(x1$lRatioTest$`Test statistic`, 0)
 
   expect_equal(x1$K, x2$K, 2)
   expect_equal(x1$n, x2$n, 172)
@@ -146,9 +228,11 @@ test_that("id.ngml 2-dim stage3 = TRUE works, with restriction", {
   x1 <- id.ngml(v1, stage3 = TRUE, restriction_matrix = restriction_matrix)
   x2 <- id.ngml_boot(v1, stage3 = TRUE, restriction_matrix = restriction_matrix)
 
-  expect_equal(round(x1$Lik, 4),round(x2$Lik, 4), -433.6669)
+  expect_equal(round(x1$Lik, 4), round(x2$Lik, 4), -433.6669)
   expect_equal(sum(round(x1$B, 4)), sum(round(x2$B, 4)), 1.8721)
   expect_equal(round(sum(x1$df),4), round(sum(x2$df),4), 8.3969)
+
+  expect_gt(x1$lRatioTest$`Test statistic`, 0)
 
   expect_equal(x1$K, x2$K, 2)
   expect_equal(x1$n, x2$n, 172)
@@ -157,3 +241,4 @@ test_that("id.ngml 2-dim stage3 = TRUE works, with restriction", {
 
   expect_match(x1$method, "Non-Gaussian maximum likelihood")
 })
+
